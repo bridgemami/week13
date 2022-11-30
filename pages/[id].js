b2cd2cdcb6277a42983267790b65ec4200abd7a7
bridgemami <mdbridgeman@gmail.com>
2022-11-29 16:04:30 -0800

    updated the endpoint

--- pages/[id].js
+++ pages/[id].js
@@ -6,49 +6,36 @@ import CharacterList from '../components/author';
 
 //create an instance of the getStaticPaths() to report next all possible dynamic urls
 export async function getStaticPaths() {
-    const paths =  await getAllIds();
-  
-    console.log(paths);
-    return {
-      paths,
-      fallback: false
-    };
-  }
+  const paths = await getAllIds();
+  return {
+    paths,
+    fallback: false
+  };
+}
 
 //create an instance of the getStaticProps() to return data for one person
 export async function getStaticProps({ params }) {
-    const itemData = await getData(params.id);
-    // console.log(itemData);
-    return {
-      props: {
-        itemData
-      }
-    };
+  const itemData = await getData(params.id);
+  // console.log(itemData);
+  return {
+    props: {
+      itemData
+    }
+  };
 }
 
 
 // make a react component to display all details about a person when a dynamic route matches, like id 1 or id 2
-export default function NoStarWars ({itemData}) {
-    return (
-        <Layout home>
-        <h1 className="text-center mb-4">The Post List</h1>
-      <div className="list-group">
-     {allData.map(({id, title, content, link, date, modify} ) => 
-     <>
-     <ul key={id}>
-     <li>The post id is <strong>{id}</strong>.</li>
-     <li>The title is <strong>{title.rendered}</strong>.</li>
-     <li>The post is <em>{content.rendered}</em></li>
-     <li>Date created: <strong>{date}</strong></li>
-     <li>Last modified: {modify}</li>
-     <li><Link href={id}><a>More Information</a></Link></li>
-
-     </ul>
-     {/* <button><li><a href=`/{id}`>More Info</a></li></button> */}
-     </>
-)
-     }
-    </div>    
-  </Layout>
-    );
+export default function Entry({ itemData }) {
+  return (
+    <Layout>
+      <article className="card col-6">
+        <div className="card-body">
+          <h5 className="card-title">{itemData.post_title}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{itemData.user_login}</h6>
+          <div className="card-text" dangerouslySetInnerHTML={{__html: itemData.post_content}} />
+        </div>
+      </article>
+    </Layout>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout from '../components/layout';
+// import Layout from '../components/layout';
 import { getSortedList } from '../lib/data';
 // import PersonSWChar from "../components/list"
 import styles from '../styles/Home.module.css'
@@ -11,30 +11,31 @@ export async function getStaticProps() {
   // const notStarWars= await getSortedList(false);
   return {
     props: {
-       allData: allData,
+      //  allData: allData,
+      allData
     }
   }
 }
 export default function Home({ allData }) {
   return (
-      <Layout home>
+      <>
             <h1 className="text-center mb-4">The Post List</h1>
           <div className="list-group">
          {allData.map(({id, title, content, link, date, modify} ) => 
          <>
          <ul key={id}>
          <li>The post id is <strong>{id}</strong>.</li>
-         <li>The title is <strong>{title.rendered}</strong>.</li>
-         <li>The post is <em>{content.rendered}</em></li>
+         <li>The title is <strong>{title}</strong>.</li>
+         {/* <li>The post is <em>{content.rendered}</em></li>
          <li>Date created: <strong>{date}</strong></li>
-         <li>Last modified: {modify}</li>
+         <li>Last modified: {modify}</li> */}
          </ul>
          <Link href={`/${id}`} key={id}><a>More Info</a></Link>
          </>
   )
          }
         </div>    
-      </Layout>
+      </>
   );
 }
 // {sw.map( ({id, author}) => <CharacterLink key={"pl"+id} tag={tag} id={id} author={author} /> )}
